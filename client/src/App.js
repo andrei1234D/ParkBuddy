9c215b68ebf1d39e21c7e17546ddc93435200340
andrei <balasandrei200@yahoo.com>
2024-06-29 16:24:05 +0300

    cmt

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div>
-      <Router>
+      <Router basename="/">
         <GlobalStatesContextProvider>
           <Particles />
           <Nav />
