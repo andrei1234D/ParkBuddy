2cc32344098cff2bb5baf8995b625e670fa4ccaa
andrei <balasandrei200@yahoo.com>
2024-06-30 19:51:58 +0300

    path fix

--- client/src/functionalElements/designs/NavTextAnimation.js
+++ client/src/functionalElements/designs/NavTextAnimation.js
@@ -11,7 +11,7 @@ export default function NavTextAnimation() {
     <div>
       {location.pathname === '/Lend-A-Spot' ? (
         <div className="normal-text">LEND A SPOT</div>
-      ) : location.pathname === '/asd' ? (
+      ) : location.pathname === '/Rent-A-Spot' ? (
         <div className="normal-text">RENT A SPOT</div>
       ) : (
         <div className="catchy">
